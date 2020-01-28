4d6939cd65cf0b288a2905af5f1b1f2a7e480779
GRBurst <GRBurst@protonmail.com>
2020-01-28 14:05:41 +0100

    remove fdroidserver as dependency

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@ let
   pkgs = import <nixpkgs> { };
   flutterPkgs = (import (builtins.fetchTarball "https://github.com/babariviere/nixpkgs/archive/flutter-init.tar.gz")  {});
   dartPkgs    = (import (builtins.fetchTarball "https://github.com/GRBurst/nixpkgs/archive/dart.tar.gz")              {});
+
 in
   pkgs.stdenv.mkDerivation {
     name = "${projectName}";
@@ -15,13 +16,13 @@ in
       dartPkgs.dart
       android-studio
       git
-      fdroidserver
     ];
 
     shellHook=''
       echo -e "\e[0;32mWelcome to ${projectName} Development\e[m"
       echo -e "It is advised to add \e[1;34mprograms.adb.enable = true;\e[m to your nix configuration"
       echo -e "Furthermore, add your user to the \e[1;34madbusers\e[m group."
+      export PATH="~/projects/fdroidserver:$PATH"
     '';
   }
 
